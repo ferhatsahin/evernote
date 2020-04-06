d7fd2b6217036a12bfa39dfa5138bbe8ebfac704
Ferhat Sahin <ferhat.sahin@akinon.com>
2020-04-06 23:03:55 +0300

    refactor app.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ export default class App extends Component {
     .onSnapshot( serverUpdate => {
       const notes = serverUpdate.docs.map( doc => ({ id: doc.id, ...doc.data()}))
       this.setState({ notes })
-      console.log(notes)
     })
   }
 
@@ -33,42 +32,39 @@ export default class App extends Component {
       .collection('notes')
       .doc(id)
       .update({
-        title: noteObj.title,
-        body: noteObj.body,
+        ...noteObj,
         timestamp: firebase.firestore.FieldValue.serverTimestamp()
       });
   }
 
   onSubmitNewNote = async (title) => {
-    const note = {
-      title: title,
-      body: ''
-    };
+    const note = { title: title, body: '' };
     const newFromDB = await firebase
       .firestore()
       .collection('notes')
       .add({
-        title: note.title,
-        body: note.body,
+        ...note,
         timestamp: firebase.firestore.FieldValue.serverTimestamp()
       });
     const newID = newFromDB.id;
-    await this.setState({ notes: [...this.state.notes, note] });
-    const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note => _note.id === newID)[0]);
-    this.setState({ selectedNote: this.state.notes[newNoteIndex], selectedNoteIndex: newNoteIndex });
+    this.setState({ notes: [...this.state.notes, note] }, () => {
+      const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note => _note.id === newID)[0]);
+      this.setState({ selectedNote: this.state.notes[newNoteIndex], selectedNoteIndex: newNoteIndex });
+    });
   }
 
   deleteNote = async (note) => {
-    const noteIndex = this.state.notes.indexOf(note);
-    await this.setState({ notes: this.state.notes.filter(_note => _note !== note) });
-    if(this.state.selectedNoteIndex === noteIndex) {
-      this.setState({ selectedNoteIndex: null, selectedNote: null });
-    } else {
-      this.state.notes.length > 1 ?
-      this.selectNote(this.state.notes[this.state.selectedNoteIndex - 1], this.state.selectedNoteIndex - 1) :
-      this.setState({ selectedNoteIndex: null, selectedNote: null });
-    }
-
+    const { notes, selectedNoteIndex } = this.state;
+    const noteIndex = notes.indexOf(note);
+    this.setState({ notes: notes.filter( noteItem => noteItem !== note) },() => {
+      if(selectedNoteIndex === noteIndex) {
+        this.setState({ selectedNoteIndex: null, selectedNote: null });
+      } else {
+        notes.length > 1 ?
+        this.selectNote(notes[selectedNoteIndex - 1], selectedNoteIndex - 1) :
+        this.setState({ selectedNoteIndex: null, selectedNote: null });
+      }
+    });
     firebase
       .firestore()
       .collection('notes')
